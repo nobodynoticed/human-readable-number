0fc7090d34f6d2195c0bc6759449870595c4c28b
nobodynoticed <eugenejukov23@gmail.com>
2021-03-10 14:56:33 +0300

    feat: add human readable number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (!number || number == 0) return "zero";
+
+    const numArr = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+    ];
+    const tens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let str = "";
+    let num = 0;
+
+    if (number >= 100) {
+        num = (number / 100) | 0;
+        str = `${numArr[num]} hundred`;
+        number -= num * 100;
+    }
+
+    if (number > 19) {
+        num = (number / 10) | 0;
+        number -= num * 10;
+        str = `${str} ${tens[num]} ${numArr[number]}`;
+    } else {
+        str = `${str} ${numArr[number]}`;
+    }
+
+    return str.trim();
+};
